ec466012f202a3fe016386cc38701801ff52191b
batgnome <fouflord@gmail.com>
2021-08-20 22:40:31 -0700

    added grid of divs and styles to draw background color when hovering over said div

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,6 +3,13 @@ grid.textContent = " this is the start";
 
 document.body.appendChild(grid);
 
-for(var i = 0; i <=16; i++) {
-    
+for(var i = 0; i <16; i++) {
+        row = document.createElement("div");
+        row.setAttribute("id", "row")
+        document.getElementById("container").appendChild(row);
+    for(var j = 0; j <16; j++) {
+        col = document.createElement("div");
+        col.setAttribute("id", "col")
+        row.appendChild(col);
+    }
 }
