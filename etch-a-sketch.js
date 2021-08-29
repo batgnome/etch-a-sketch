ad90393a8557c562c511aa0121a7f6fb17d4d5ac
batgnome <fouflord@gmail.com>
2021-08-29 07:42:21 -0700

    added buttons and size changing capabilities

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,40 @@
-grid = document.createElement("div");
-grid.textContent = " this is the start";
-
-document.body.appendChild(grid);
-
-for(var i = 0; i <16; i++) {
+function createCanvas(width = 16){
+  
+    document.getElementById('container').setAttribute(
+        'style', 
+            `width: ${(width*16)}px;
+             height: ${(width*16)}px;`
+    ); 
+    for(var i = 0; i < width; i++) {
         row = document.createElement("div");
-        row.setAttribute("id", "row")
+        row.setAttribute("id", "row");
         document.getElementById("container").appendChild(row);
-    for(var j = 0; j <16; j++) {
-        col = document.createElement("div");
-        col.setAttribute("id", "col")
-        row.appendChild(col);
+        
+        for(var j = 0; j < width; j++) {
+            col = document.createElement("div");
+            col.setAttribute("id", i+j);
+            col.setAttribute("class", "col");
+            row.appendChild(col);
+        }
     }
 }
+
+function deleteCanvas() {
+
+}
+function clearBoard() {
+    console.log("clear");
+    divs = document.querySelectorAll('div.col');
+    divs.forEach((e)=>{
+        e.setAttribute("style", "background-color: white"); 
+    })
+}
+createCanvas(32);
+divs = document.querySelectorAll('div.col');
+divs.forEach( (e)=>{
+    e.addEventListener("mouseover", ()=> {
+        
+        e.setAttribute('style', "background-color: black")
+    });
+        
+});
